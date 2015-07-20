515dc702de70e4397ad2378810bf1212f283cd6d
Anastassiya <aneznanova@aneznanova-ubuntu>
2015-07-20 18:04:44 -0400

    filtered by elementary and k-8school and factored achievement and environment

--- schoolratings.R
+++ schoolratings.R
@@ -22,73 +22,16 @@ colnames(schooldirectory)[1] <- "DBN"
 #Merge left
 schooldata <- merge(x = schooltarget, y = schooldirectory, by = "DBN", all.x = TRUE)
 
-#Getting the number of school types
-table(schooldata$`School Type`)
+schooldata<-filter(schooldata, `School Type` == 'Elementary'|`School Type` == 'K-8')
 
-#Plotting school types, boring black and white
-barplot(table(schooldata$`School Type`), ylim = c(0, 700), main = "Number of Schools by Type", ylab = "Number of Schools", xlab = "Types of schools")
+env_levels <- c("Not Meeting Target","Approaching Target","Meeting Target","Exceeding Target")
 
-#Beautiful bar with colors by the rainbow, must use, this looks nice and not boring
-qplot(factor(`School Type`), data=schooldata, geom="bar", fill=factor(`School Type`), xlab = "School Type", ylab = "Number of Schools", main = "Number of Schools by Type") + theme(legend.position='none')
+#Changing into numeric Environment Rating
+schooldata <- schooldata %>%
+  mutate("Environment Rating"=ifelse(`Environment Rating` == "N/A", NA, `Environment Rating`),
+         "Environment Rating"=factor(`Environment Rating`, env_levels))
 
-#################################
-#PLOTTING
-#################################
-
-#Getting the number of schools by achievement rating
-table(schooldata$`Achievement Rating`)
-
-#Boring plot
-barplot(table(schooldata$`Achievement Rating`), ylim = c(0, 700), main = "Number of Schools by Achievement Rating", ylab = "Number of Schools", xlab = "Target")
-
-#Plot with beautiful colors ~ achievement
-qplot(factor(`Achievement Rating`), data=schooldata, geom="bar", fill=factor(`Achievement Rating`), xlab = "Target", ylab = "Number of Schools", main = "Number of Schools by Achievement Rating") + theme(legend.position='none')
-########################
-########################
-
-#Getting the number of schools by environment
-table(schooldata$`Environment Rating`)
-
-#Boring plot of environemnt 
-barplot(table(schooldata$`Environment Rating`), ylim = c(0, 700), main = "Number of Schools by Environment Rating", ylab = "Number of Schools", xlab = "Target")
-
-#Plot with beautiful colors environment
-qplot(factor(`Environment Rating`), data=schooldata, geom="bar", fill=factor(`Environment Rating`), xlab = "Target", ylab = "Number of Schools", main = "Number of Schools by Environment Rating") + theme(legend.position='none')
-
-#Plotting the different environment by zip code. 
-qplot(factor(`Environment Rating`), data=schooldata, geom="bar") + facet_wrap(~ `Zip`)
-
-##########################
-#TO DO 
-##########################
-
-#Make scatter plot of environment and achievement rating
-plot(schooldata$`Environment Rating`, schooldata$`Achievement Rating`)
-
-#Park Slope Comparison
-
-parkslope <- filter(schooldata, Zip == c(11217, 11215))
-qplot(factor(`Environment Rating`), data=parkslope, fill=factor(`Zip`))
-
-#Achievement by school type
-#converting environment and achievement to numerics (1,2,3,4,5)
-l=unique(c(as.character(schooldata$`Environment Rating`), as.character(schooldata$`Achievement Rating`)))
-
-environments <- data.frame(Cardinal_Environment=as.numeric(factor(schooldata$`Environment Rating`, levels=l)), 
-                           Cardinal_Achievement=as.numeric(factor(schooldata$`Achievement Rating`, levels=l)))
-
-#attach environment and achievement back to schooldata
-attach_toSchool_data <- cbind(schooldata, environments)
-
-#group by geographical zip code to find the mean of environment and achievement
-environment_achievement<-aggregate(attach_toSchool_data[, 60:61], 
-                                    list(attach_toSchool_data$Geographical.District.Code), mean)
-
-#plotting Achievement vs Environment
-ggplot(environment_achievement, aes(x=Cardinal_Environment, y=Cardinal_Achievement, 
-                                    color = factor(Group.1)), size = Cardinal_Achievement) + 
-  geom_point() + 
-  geom_abline() +
-  xlab("Environment Rating") +
-  ylab("Achievement Rating") +
-  ggtitle("Achievement vs Environment rating by geo zip")
+#Changing into numeric Achievement Rating
+schooldata <- schooldata %>%
+  mutate("Achievement Rating"=ifelse(`Achievement Rating` == "N/A", NA, `Achievement Rating`),
+         "Achievement Rating"=factor(`Achievement Rating`, env_levels))
