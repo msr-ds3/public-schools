d5c58e19154fec463c311b3ec72bc2e83f69c4fb
Anastassiya <aneznanova@aneznanova-ubuntu>
2015-07-22 17:40:22 -0400

    poverty versus math andenglish test

--- scatterplots_race.R
+++ scatterplots_race.R
@@ -24,3 +24,40 @@ Asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Asian`)) +
   geom_smooth(method=lm, se=FALSE)
 
 ggplot2.multiplot(White, Hispanic, Black, Asian, cols = 2)
+
+
+#Scatterplot Race vs poverty by Math mean scale score
+math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Asian`, color = `Mean Scale Score Math`)) +
+  geom_point(shape=1) +    ggtitle("Percent Asian") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+math_black <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Black`, color = `Mean Scale Score Math`)) +
+  geom_point(shape=1) +    ggtitle("Percent Black") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+math_white <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% White`, color = `Mean Scale Score Math`)) +
+  geom_point(shape=1) +    ggtitle("Percent White") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+math_hispanic <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Hispanic`, color = `Mean Scale Score Math`)) +
+  geom_point(shape=1) +    ggtitle("Percent Hispanic") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+#Scatterplot Race vs poverty by Math mean scale score
+eng_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Asian`, color = `Mean Scale Score English`)) +
+  geom_point(shape=1) +    ggtitle("Percent Asian") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+eng_black <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Black`, color = `Mean Scale Score English`)) +
+  geom_point(shape=1) +    ggtitle("Percent Black") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+eng_white <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% White`, color = `Mean Scale Score English`)) +
+  geom_point(shape=1) +    ggtitle("Percent White") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+eng_hispanic <-math_asian <- ggplot(schooldata, aes(x=`% Poverty`, y=`% Hispanic`, color = `Mean Scale Score English`)) +
+  geom_point(shape=1) +    ggtitle("Percent Hispanic") + ylim(0.0, 1.0) +
+  geom_smooth(method=lm, se=FALSE)
+
+ggplot2.multiplot(math_asian, math_black, math_white, math_hispanic, eng_asian, eng_black, eng_white, eng_hispanic, col = 4)
