a179deff6a73ab93b0db40c6d2dda347fb718289
Glenda <gascencio@student.sjcny.edu>
2015-07-22 13:58:04 -0400

    This is the Sold Listing in Manhattan R Analyses File

--- Sold_listing_in_Manhattan/sold_listing_in_manhattan.R
+++ Sold_listing_in_Manhattan/sold_listing_in_manhattan.R
@@ -1,62 +1,12 @@
-#By: Glenda Ascencio                                                     July 22, 2015
+#By: Glenda Ascencio, Riley H                                             July 22, 2015
 
-#########################################################################################
-##                                     Sold Listing in NYC                             ##
-#########################################################################################
 
-##loading libraries
+
 library(dplyr)
 library(ggplot2)
 
-##  Loading the Sold Listing in NYC
-data_dir <- "."
-
-## Renaming the first row name
-vector <- c('url','unit_type','price','status','neighborhood','borough','school_name',
-            'address_num', 'address_street', 'unit', 'city', 'zip_code', 'latitude', 
-            'longitude', 'building_desc', 'sqft','bedrooms','baths', 'total_rooms', 
-            'maintenance_fee','taxes', 'start_date', 'end_date', 'days_on_the_market', 
-            'source', 'listing_agent','closing_price', 'tax_type', 'tax_expiration') 
-
-sold_listing_in_manh <- data.frame(read.csv("*/manhattan_sold.csv"), stringsAsFactors = FALSE, sep=',')
-colnames(sold_listing_in_manh) <- vector
-
-##View the sold_listing
-View(sold_listing_in_manh)
-names(sold_listing_in_manh)
-
-########################################################################################
-##                         Basic Statistics On The Sold Listing in NYC                ##
-#########################################################################################
-
-##Take some statistics from the sold listing file in NYC
-summary(sold_listing_in_manh)
-
-#######################################################################################
-##                          Data Cleaning                                           ##
-######################################################################################
-
-
-## Removing all the useless data, that finds any NA values or sqft = 0
-############ Give this a new name to reflect what it stores ################
-sold_manh_cleaned <- sold_listing_in_manh[!is.na(sold_listing_in_manh$bedrooms), ]
-sold_manh_cleaned <- sold_manh_cleaned[!is.na(sold_manh_cleaned$sqft), ]
-sold_manh_cleaned <- sold_manh_cleaned[sold_manh_cleaned$sqft > 0, ]
-sold_manh_cleaned <- sold_manh_cleaned[0:-1, 0:29]
-
-## Incrementing bedrooms by 1 so that a studio is "one room", and 1 br is "1 br+1 room"
-sold_manh_cleaned$bedrooms <- sold_manh_cleaned$bedrooms + 1
-
-## Add avg price per square foot as a column
-sold_manh_cleaned$price_per_sqft <- sold_manh_cleaned$price/sold_manh_cleaned$sqft
-
-## Add avg price per bedroom as a column
-sold_manh_cleaned$price_per_bed <- sold_manh_cleaned$price/sold_manh_cleaned$bedrooms
-
-View(sold_manh_cleaned)
-
-##### SAVE THE FILES
-save(sold_listing_in_manh, sold_manh_cleaned, file = sprintf('%s/SoldInManhData.RData', data_dir))
+##### Load Listings for Manhattan
+load(sprintf('%s/SoldInManhData.RData', data_dir))
 
 ########################################################################################
 ##                  Answering Some Questions On The Manhattan Sold Listing Data       ##
@@ -126,18 +76,7 @@ ggplot(data = listing_per_sz_and_ut, aes(x = school_name, y = per_sz_and_ut, col
 ###9) Find the average price per bedroom
 
 aver_price_per_bedroom <- sold_manh_cleaned %>% 
-group_by(school_name, bedrooms) %>%
-  summarise(price_per_bed = n())
+group_by(bedrooms) %>%
+  summarise(bed_price = mean(price_per_room))
 
 View(aver_price_per_bedroom)
-
-###10) Plot the average price per bedroom
-ggplot(data = aver_price_per_bedroom, aes(x = school_name, y = price_per_bed, color = bedrooms)) + 
-  geom_point()+
-  xlab('School Names') + 
-  ylab('Average Price Per Bedroom') + 
-  ggtitle('Average Price Per Bedroom By School Zone')+
-  theme(legend.title=element_blank(),
-        axis.text.x=element_text(angle=80, hjust=1), 
-        legend.background = element_rect(fill = "transparent"))
-
