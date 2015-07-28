956d96da0c50d67727bf956c6a5512b62447b5c9
Anastassiya <aneznanova@aneznanova-ubuntu>
2015-07-28 14:27:27 -0400

    updated file with dplyr library and changed variable names

--- load_schooldata_complist_soldlist.R
+++ load_schooldata_complist_soldlist.R
@@ -1,7 +1,8 @@
+library(dplyr)
 load('../public-schools/schools.RData') #gives schooldata file
 load('../public-schools/streeteasy/streeteasy_sales.RData') #gives complete_listing and sold_listing data files
 #to get rid on NAs and filter some unnecessary data
-df <- complete_listings[!is.na(complete_listings$bedrooms) & 
+filter_listings <- complete_listings[!is.na(complete_listings$bedrooms) & 
                           !is.na(complete_listings$baths) & 
                           !is.na(complete_listings$DBN) & 
                           !is.na(complete_listings$borough) & 
@@ -14,6 +15,6 @@ df <- complete_listings[!is.na(complete_listings$bedrooms) &
                           complete_listings$bedrooms >= 0 & 
                           complete_listings$bedrooms < 4, ]
 # add DBN to tthe table and join df with schooldata
-df <- mutate(df, DBN = droplevels(DBN))
-schooldata1<-inner_join(schooldata, df, by = "DBN")
-save(schooldata1, file="compl_school.RData")
+filter_listings <- mutate(filter_listings, DBN = droplevels(DBN))
+schools_zone_sales<-inner_join(schooldata, filter_listings, by = "DBN")
+save(schools_zone_sales, file="compl_school.RData")
