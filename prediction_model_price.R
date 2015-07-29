072b788c6d6b6e7b9fb322d79310a41ac195fc4e
Anastassiya <aneznanova@aneznanova-ubuntu>
2015-07-29 10:49:23 -0400

    model prediction for the price

---
+++ prediction_model_price.R
@@ -0,0 +1,48 @@
+
+
+schools_zone_sales <- schools_zone_sales %>% mutate(DBN = as.factor(DBN))
+schools_zone_sales <-filter(schools_zone_sales, sqft >100, sqft<2000, price_per_sqft < 4000)
+schools_zone_sales$mean=rowMeans(schools_zone_sales[,c("Mean Scale Score Math", "Mean Scale Score English")], na.rm=TRUE)
+# randomly select 80% of the data to train the model on
+# and 20% to test on
+set.seed(22)
+num_train <- round(nrow(schools_zone_sales) * 0.75)
+ndx <- sample(nrow(schools_zone_sales), num_train)
+train <- schools_zone_sales[ndx, ]
+test <- schools_zone_sales[-ndx, ]
+test <- test[test$DBN %in% train$DBN, ]
+test <- test[test$neighborhood %in% train$neighborhood, ]
+
+rm(ndx, num_train, complete_listings, schooldata, sold_listings)
+
+school.lm <-lm(price_per_sqft ~ bedrooms + baths + mean + DBN, data = train)
+summary(school.lm)
+test$predict_test <- predict(school.lm, test)
+#ggplot with point and line
+ggplot(test, aes(x=price_per_sqft,y=predict_test)) +  xlab("Actual ") +
+ylab("Predicted") + ggtitle("Actual vs Predicted Test Scores") + geom_abline()+
+geom_point(aes(color = `Environment Rating`))
+
+
+x = model.matrix(I(price_per_sqft ) ~ bedrooms + baths + mean + DBN + `% Poverty` + `% White` + `% Hispanic` + `% Asian` + neighborhood, data = train)
+y = train$price_per_sqft 
+cvfit = cv.glmnet (x, y)
+
+xt =  model.matrix(I(price_per_sqft ) ~ bedrooms + baths + mean + DBN + `% Poverty` + `% White`  + `% Hispanic` + `% Asian`  + neighborhood, data = test)
+yt = test$price_per_sqft 
+cvpred <- predict(cvfit, newx = xt, s = "lambda.min")
+
+## Change prediction to easy view
+cvpred <- data.frame(cvpred)
+## Get correlation between the predictions and the truth
+cor(cvpred, yt)^2
+## Plot the cvfit to see the the lambda  and mean square error
+plot(cvfit)
+
+coef(cvfit, s = "lambda.min")
+
+sqrt(mean((cvpred$X1 - yt)^2))
+
+ggplot(cvpred, aes(y=X1, x=yt)) +  xlab("Actual ") +
+  ylab("Predicted") + ggtitle("Actual vs Predicted Test Scores") + geom_abline()+
+  geom_point()
