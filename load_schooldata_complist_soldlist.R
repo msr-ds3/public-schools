7696a4c39d4f5a14428bd719a027bafcdf5a9210
Anastassiya <aneznanova@aneznanova-ubuntu>
2015-07-31 11:13:31 -0400

    updated file, eliminated price_per_sqft<50000

--- load_schooldata_complist_soldlist.R
+++ load_schooldata_complist_soldlist.R
@@ -10,8 +10,7 @@ filter_listings <- complete_listings[!is.na(complete_listings$bedrooms) &
                           !is.na(complete_listings$price_per_sqft) & 
                           complete_listings$baths < 4  & 
                           complete_listings$baths > 0 & 
-                          complete_listings$sqft < 50000 & 
-                          complete_listings$price_per_sqft > 100  & 
+                          complete_listings$sqft > 100  & 
                           complete_listings$bedrooms >= 0 & 
                           complete_listings$bedrooms < 4, ]
 # add DBN to tthe table and join df with schooldata
@@ -19,3 +18,5 @@ filter_listings <- mutate(filter_listings, DBN = droplevels(DBN))
 schools_zone_sales<-inner_join(schooldata, filter_listings, by = "DBN")
 rm(complete_listings,sold_listings,schooldata, filter_listings)
 save(schools_zone_sales, file="compl_school.RData")
+
+
